608aa8e4b40a072c27e744de9abc5bd88b9d4a5d
Robbie Weinel <robbie.weinel@gmail.com>
2019-10-10 22:23:21 -0600

    Making Original Cleaner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import "./index.css";
-import logo from "./logo.png";
 
 class App extends Component {
   render() {
@@ -9,7 +8,6 @@ class App extends Component {
       <MDBContainer>
         <MDBRow center style={{ height: "100vh" }}>
           <MDBCol middle="true" sm="8" className="text-center">
-            <img src={logo} alt="logo" style={{ width: "10rem" }} />
             <h1>Welcome to Your MDB React App</h1>
             <p className="mb-2">The application is configured and ready to import our components.</p>
             <MDBBtn href="https://mdbootstrap.com/docs/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></MDBBtn>
